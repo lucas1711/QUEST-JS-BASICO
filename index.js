c10b3a3e0de93fa01f109ba41cb0fed5afb2d30b
lucas costa <lucas34474040@gmail.com>
2022-05-25 15:47:32 -0300

    quest javascript basico

---
+++ index.js
@@ -0,0 +1,28 @@
+function somar(numero1, numero2){
+    let soma = numero1 + numero2;
+    return console.log('A soma dos dois números é:' + soma);
+}
+
+somar(50,10)
+
+
+function subtracao(numero1, numero2){
+let subtrair = numero1 - numero2;
+return console.log('A subtracao dois números é:' + subtrair);
+}
+
+subtracao(50,30)
+
+function divisao(numero1, numero2){
+let dividir = numero1 / numero2;
+return console.log('A divisão dois números é:' + dividir);
+}
+
+divisao(100,20)
+
+function multiplicacao(numero1, numero2){
+let multiplicar = numero1 * numero2;
+return console.log('A multiplicacao dois números é:' + multiplicar);
+}
+
+multiplicacao(30,2)
